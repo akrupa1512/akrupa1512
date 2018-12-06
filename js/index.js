7033b7ec6ec6f8f1ebdfe73fa5d6559863edc73e
kaG1512kag <rassor96@gmail.com>
2018-12-06 18:11:30 +0200

    fix adaptability for mobile devices

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
     document.getElementById("card-info").style.flexDirection = "column";
+    document.getElementById("card-info").style.height = "450px";
+    document.getElementById("photo").style.width = "auto";
 }
