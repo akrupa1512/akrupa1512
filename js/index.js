11dbc8c6f43ae77267b36677471892ea9b06fa04
akrupa <rassor96@gmail.com>
2019-10-30 18:28:33 +0200

    improvements

--- js/index.js
+++
@@ -1,5 +0,0 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    document.getElementById("card-info").style.flexDirection = "column";
-    document.getElementById("card-info").style.height = "450px";
-    document.getElementById("photo").style.width = "auto";
-}
